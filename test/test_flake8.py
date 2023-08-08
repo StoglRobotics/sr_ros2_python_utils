1f0712e8f943a6ec3497746e6da8fbeaae9e7162
Guillaume Walck <guillaume.walck@stoglrobotics.de>
2023-08-08 18:48:37 +0200

    ROSified the package

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Stogl Robotics
+# Copyright 2017 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ament_flake8.main import main_with_errors
+import pytest
+
+
+@pytest.mark.flake8
+@pytest.mark.linter
+def test_flake8():
+    rc, errors = main_with_errors(argv=[])
+    assert rc == 0, \
+        'Found %d code style errors / warnings:\n' % len(errors) + \
+        '\n'.join(errors)
