8cd3edf359c7b0638a99ee167ffb905cfd894ee5
Dr. Denis <denis@stoglrobotics.de>
2023-09-14 16:42:30 +0200

    Rosifying the package for ROS-standard module import (#4)
    Co-authored-by: Guillaume Walck <guillaume.walck@stoglrobotics.de>

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'sr_ros2_python_utils'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dr. Denis Štogl',
+    maintainer_email='denis@stoglrobotics.de',
+    description='Python Utils for ROS 2 you might find useful!',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
