a5f5222324d19bdcd0055bdb68738d04cdfc27ea
SahWag <65761770+SahWag@users.noreply.github.com>
2023-09-26 15:51:41 +0200

    Add python transform util script (#5)
    * add a quaternion_to_euler helper script
    * add a python cli transformer script
    * Rename helper script to generic name

--- sr_ros2_python_utils/transforms.py
+++ sr_ros2_python_utils/transforms.py
@@ -24,7 +24,31 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import Pose, Vector3, Vector3Stamped
+from numpy import arctan2, arcsin
 
+def _euler_from_quaternion(quaternion):
+    """
+    Converts quaternion (w in last place) to euler roll, pitch, yaw
+    quaternion = [x, y, z, w]
+    Bellow should be replaced when porting for ROS 2 Python tf_conversions is done.
+    """
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.w
+
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = arctan2(sinr_cosp, cosr_cosp)
+
+    sinp = 2 * (w * y - z * x)
+    pitch = arcsin(sinp)
+
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = arctan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
 
 def _quaternion_from_euler(ai:float, aj:float, ak:float):
     # quaternion order is [qx, qy, qz, qw]
